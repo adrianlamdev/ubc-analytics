ecc3bf0b2add7ba3b5342917b5536762432e10f2
Adrian Lam <adrianlamdev@gmail.com>
2024-12-20 18:07:23 -0500

    chore: remove testing features in jupyter notebook

--- analysis/deploy.py
+++ analysis/deploy.py
@@ -1,47 +1,3 @@
-# from transformers import Pipeline
-# import pandas as pd
-# from joblib import load
-# import os
-# from utils.inference_utils import prepare_inference_features
-
-# class GradePredictionPipeline(Pipeline):
-#     def __init__(self):
-#         super().__init__()
-
-#         model_path = os.path.join("models", "ubcv_grade_predictor_ridge.joblib")
-#         self.model = load(model_path)
-
-#         self.df = pd.read_csv("../data/processed/ubcv_grades_processed_tableau_all.csv")
-#         self.prepare_inference_features = prepare_inference_features
-
-#     def preprocess(self, inputs):
-#         # Validate inputs
-#         required_fields = ['subject', 'course', 'year']
-#         for field in required_fields:
-#             if field not in inputs:
-#                 raise ValueError(f"Missing required field: {field}")
-        
-#         # Create features
-#         features = self.prepare_inference_features(
-#             df=self.df,
-#             subject=inputs['subject'],
-#             course=inputs['course'],
-#             year=inputs['year'],
-#             campus=inputs.get('campus', 'UBCV'),
-#             session=inputs.get('session', 'W'),
-#             professor=inputs.get('professor', '')
-#         )
-#         return features
-    
-#     def _forward(self, model_inputs):
-#         return self.model.predict(model_inputs)
-    
-#     def postprocess(self, model_outputs):
-#         return {
-#             "predicted_grade": float(model_outputs[0]),
-#             "status": "success"
-#         }
-
 import sklearn
 from sklearn.tree import DecisionTreeClassifier
 from skops import hub_utils, card
@@ -53,7 +9,6 @@ import pandas as pd
 df = pd.read_csv("data/processed/ubcv_grades_processed_tableau_all.csv")
 
 # Load example input
-
 example_input = prepare_inference_features(
             df=df,
             subject = 'CPSC',
